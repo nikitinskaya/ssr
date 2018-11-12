2545c1a7ad6059315e894df5a99587b282cbe1b8
Vera Nikitinskaya <nikitinskaya.v@gmail.com>
2018-11-12 18:17:46 +0300

    Add step 1

---
+++ src/server/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import { renderToString } from "react-dom/server";
+import App from "../shared/App";
+import React from "react";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static("public"));
+
+app.get("*", (req, res, next) => {
+        const markup = renderToString(
+            <App/>
+        )
+        res.send(
+            `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <title>SSR</title>
+        </head>
+        <body>
+          <div id="app">${markup}</div>
+        </body>
+        </html>`
+        )
+    }
+)
+
+app.listen(3010, () => {
+    console.log("We are on 3010")
+});
+
